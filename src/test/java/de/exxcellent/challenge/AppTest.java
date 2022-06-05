6839a760eadb2232bdad81cbc95dd739cd3dd5d1
tabea kirchner <t.kirchner@fz-juelich.de>
2022-06-05 20:06:47 +0200

    football difference: remove unused imports

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -3,11 +3,9 @@ package de.exxcellent.challenge;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class AppTest {
